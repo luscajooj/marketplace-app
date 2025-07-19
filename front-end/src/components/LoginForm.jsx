ecf8a839785a284d7e6672b8004180fc7f3aaa7e
Lucas Ramos <lucas.ramos.guimaraes@gmail.com>
2025-07-19 18:59:17 -0300

    Estilo LoginForm iniciado

--- front-end/src/components/LoginForm.jsx
+++ front-end/src/components/LoginForm.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import "./LoginForm.css";
+
+import mail from "../assets/mail.png";
+import access from "../assets/access.png";
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
-  )
-}
+    <div className="form_container">
+      <div className="form_header">
+        <h1>Acesse sua conta</h1>
+        <p>Informe seu e-mail e senha para entrar</p>
+      </div>
 
-export default LoginForm
+      <div className="form_label">
+        <label htmlFor="email">
+          <h1>E-mail</h1>
+          <span>
+            <img src={mail} alt="" />
+          <p>Seu e-mail cadastrado</p>
+          </span>
+          <input className="form_input" type="text" name="email" id="email" />
+        </label>
+        <label htmlFor="password">
+          <h1>Senha</h1>
+          <span>
+            <img src={access} alt="" />
+          <p>Sua senha de acesso</p>
+          </span>
+          <input className="form_input" type="password" name="password" id="password" />
+        </label>
+      </div>
+      <input type="submit" className="login_button" value="Acessar"></input>
+
+    </div>
+  );
+};
+
+export default LoginForm;
