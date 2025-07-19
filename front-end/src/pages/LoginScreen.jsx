094d42533f65d5332c3b23db345aff09c14a6e73
Lucas Ramos <lucas.ramos.guimaraes@gmail.com>
2025-07-19 17:31:26 -0300

    LoginForm component criado

--- front-end/src/pages/LoginScreen.jsx
+++ front-end/src/pages/LoginScreen.jsx
@@ -1,13 +1,14 @@
 // Style
 import "./LoginScreen.css";
 import logo from "../assets/Title.png";
+import LoginForm from "../components/LoginForm";
 
 const LoginScreen = () => {
   return (
     <div className="container">
       <img src={logo} alt="logo" className="logo" />
 
-      
+      <LoginForm />
     </div>
   );
 };
