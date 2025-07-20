55960784e82b10b5f10ddddc86943996e5dde9e3
Lucas Ramos <lucas.ramos.guimaraes@gmail.com>
2025-07-20 14:52:19 -0300

    Estilo LoginScreen finalizado

--- front-end/src/components/LoginForm.jsx
+++ front-end/src/components/LoginForm.jsx
@@ -1,36 +1,78 @@
+import { useState } from "react";
 import "./LoginForm.css";
 
 import mail from "../assets/mail.png";
 import access from "../assets/access.png";
+import view from "../assets/view.png";
 
 const LoginForm = () => {
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [emailValue, setEmailValue] = useState("");
+
+  const [passwordFocused, setPasswordFocused] = useState(false);
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const shouldHideEmailLabel = emailFocused || emailValue.trim() !== "";
+  const shouldHidePasswordLabel =
+    passwordFocused || passwordValue.trim() !== "";
+
   return (
     <div className="form_container">
       <div className="form_header">
         <h1>Acesse sua conta</h1>
         <p>Informe seu e-mail e senha para entrar</p>
       </div>
+      <form action="">
+        <div className="form_label">
+          <label htmlFor="email">
+            <h1>E-mail</h1>
+            <span className={shouldHideEmailLabel ? "fade hidden" : "fade"}>
+              <img src={mail} alt="" />
+              <p>Seu e-mail cadastrado</p>
+            </span>
+            <input
+              className="form_input"
+              type="email"
+              name="email"
+              id="email"
+              value={emailValue}
+              onFocus={() => setEmailFocused(true)}
+              onBlur={() => setEmailFocused(false)}
+              onChange={(e) => setEmailValue(e.target.value)}
+              required
+            />
+          </label>
 
-      <div className="form_label">
-        <label htmlFor="email">
-          <h1>E-mail</h1>
-          <span>
-            <img src={mail} alt="" />
-          <p>Seu e-mail cadastrado</p>
-          </span>
-          <input className="form_input" type="text" name="email" id="email" />
-        </label>
-        <label htmlFor="password">
-          <h1>Senha</h1>
-          <span>
-            <img src={access} alt="" />
-          <p>Sua senha de acesso</p>
-          </span>
-          <input className="form_input" type="password" name="password" id="password" />
-        </label>
-      </div>
-      <input type="submit" className="login_button" value="Acessar"></input>
+          <label htmlFor="password">
+            <h1>Senha</h1>
+            <span
+              className={`form_label-pass_span fade ${
+                shouldHidePasswordLabel ? "hidden" : ""
+              }`}
+            >
+              <img src={access} alt="" />
+              <p>Sua senha de acesso</p>
+            </span>
+            <img src={view} alt="" className={`view`} />
+            <input
+              className="form_input"
+              type="password"
+              name="password"
+              id="password"
+              value={passwordValue}
+              onFocus={() => setPasswordFocused(true)}
+              onBlur={() => setPasswordFocused(false)}
+              onChange={(e) => setPasswordValue(e.target.value)}
+              required
+            />
+          </label>
+        </div>
 
+        <button type="submit" className="login_button">
+          <span>Acessar</span>
+          <span>→</span>
+        </button>
+      </form>
     </div>
   );
 };
--- front-end/src/pages/LoginScreen.jsx
+++ front-end/src/pages/LoginScreen.jsx
@@ -1,12 +1,14 @@
 // Style
 import "./LoginScreen.css";
-import logo from "../assets/Title.png";
 import LoginForm from "../components/LoginForm";
+import logo from "../assets/Title.png";
+import background from "../assets/Background.png";
 
 const LoginScreen = () => {
   return (
     <div className="container">
       <img src={logo} alt="logo" className="logo" />
+      <img src={background} alt="background" className="background" />
 
       <LoginForm />
     </div>
